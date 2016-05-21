fbf4e36e39e4fbb61af7bb0d2ea3e9146f276a10
maizhiying <zhiying.mai@gmail.com>
2016-05-21 21:09:50 +0800

    修改构建配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.join(__dirname, '/build'),
-    publicPath: '/assets/',
     filename: 'bundle.js'
   },
   devtool: 'source-map',
