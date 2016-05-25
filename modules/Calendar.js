7e818c14da20f679c3c8bdc56a918b4876474147
maizhiying <zhiying.mai@gmail.com>
2016-05-25 16:26:10 +0800

    加入异步reducer的支持

--- modules/Calendar.js
+++ modules/Calendar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
 import CalendarContainer from '../containers/Calendar'
 import calendarApp from '../reducers/Calendar'
@@ -8,7 +9,7 @@ import calendarApp from '../reducers/Calendar'
 import createLogger from 'redux-logger'
 
 const loggerMiddleware = createLogger()
-const createStoreWithMiddleware = applyMiddleware(loggerMiddleware)(createStore)
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, loggerMiddleware)(createStore)
 
 let store = createStoreWithMiddleware(calendarApp)
 
